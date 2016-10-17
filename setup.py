163b8e81fb9b0d5f56bbac028c1bc78dfb8151db
carlosplf <carlosplfilho@gmail.com>
2016-10-17 14:54:24 -0200

    Add setup.py.

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packes
+from setuptools import setup
+
+setup=(
+    name='big-data-orm',
+    version='0.1',
+    packages=find_packes()
+    install_requires=[
+        'google-api-python-client'
+    ]
+)
