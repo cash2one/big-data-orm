907381a4f8723b17b1eb5115f3c4a6d2653eecf8
carlosplf <carlosplfilho@gmail.com>
2016-10-17 16:10:17 -0200

    Fix basic test.

--- examples/basic_test.py
+++ examples/basic_test.py
@@ -2,11 +2,11 @@ from big_data_orm.big_query_connector.session import Session
 from big_data_orm.adwords_models.adwords_keyword import AdwordsKeyword
 
 
-s = Session(project_id='bigdata-141819', dataset_id='adwords_report_data',
-            storage_file='/home/carlos/credentials_storage.dat')
+s = Session(project_id='', dataset_id='',
+            storage_file='credentials_storage.dat')
 s.connect()
 
-print "================ Query 2 ===================="
+print "================ Query 1 ===================="
 k = AdwordsKeyword()
 q = k.query(k.keyword_id, k.cost, k.keyword).order_by(k.cost, desc=True).limit(100)
 print q.all(s, newest_only=True, filter_key='keyword_id')
