9b89c6e110a87899060ca8e17d3741ff25fadd42
Carlos Pereira Lopes Filho <carlosplfilho@gmail.com>
2017-02-16 15:14:38 -0200

    New stable version release.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='big-data-orm',
-    version='0.1.2',
+    version='0.2.0',
     packages=find_packages(),
     install_requires=[
         'oauth2client < 4.0.0',
